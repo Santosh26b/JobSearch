2529f7f4e7d2dae4090f0196f1fe310ed3e0016e
Santosh26b <santoshbudagavi@gmail.com>
2026-01-28 11:21:46 +0530

    Initiazed the project

---
+++ src/test/java/com/santosh/JobSearch/JobSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosh.JobSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
