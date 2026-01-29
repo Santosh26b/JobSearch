dd921bebd3269a2dacc0fb1c1b91a4815c313abc
Santosh26b <santoshbudagavi@gmail.com>
2026-01-29 10:40:47 +0530

    Completed

---
+++ src/main/java/com/santosh/JobSearch/service/JobService.java
@@ -0,0 +1,23 @@
+package com.santosh.JobSearch.service;
+
+import com.santosh.JobSearch.model.JobPost;
+import com.santosh.JobSearch.repository.JobRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobService {
+
+    @Autowired
+    private JobRepo repo;
+
+    public void addJob(JobPost jobPost) {
+        repo.addJob(jobPost);
+    }
+
+    public List<JobPost> getAllJobs() {
+        return repo.getAllJobs();
+    }
+}
